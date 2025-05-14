bfc156bb1cbd8a16211338753d2f59e6419b0286
hry2 <79210445+hry2@users.noreply.github.com>
2025-05-14 10:34:00 +0200

    Update service.py

--- movie-service/app/api/service.py
+++ movie-service/app/api/service.py
@@ -1,9 +1,10 @@
 import os
 import httpx
 
-CAST_SERVICE_HOST_URL = 'http://localhost:8002/api/v1/casts/'
+# Valeur par défaut utilisable localement
+DEFAULT_CAST_SERVICE_HOST_URL = 'http://localhost:8002/api/v1/casts/'
 
 def is_cast_present(cast_id: int):
-    url = os.environ.get('CAST_SERVICE_HOST_URL') or CAST_SERVICE_HOST_URL
+    url = os.environ.get('CAST_SERVICE_HOST_URL', DEFAULT_CAST_SERVICE_HOST_URL)
     r = httpx.get(f'{url}{cast_id}')
-    return True if r.status_code == 200 else False
+    return r.status_code == 200
